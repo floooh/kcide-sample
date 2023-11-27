c3acb4b1a279795ee7d5b6e60aa8216cd2f8e5a9
Andre Weissflog <floooh@gmail.com>
2023-11-27 18:27:07 +0100

    start coding something less trivial

--- src/main.asm
+++ src/main.asm
@@ -1,5 +1,32 @@
     org 200h
-    ld a,5
-    ld b,6
-    add a,b
+    ld de,0
+    call clear_screen
+    ret
+
+clear_screen:
+    macro pde4
+    push de
+    push de
+    push de
+    push de
+    endm
+
+    di
+    ld hl,0
+    add hl,sp               ; store SP into HL
+    ld sp,A800h             ; load SP with end of video ram
+    ld b,40*4
+clear_screen_loop:
+    ; 64 bytes
+    pde4
+    pde4
+    pde4
+    pde4
+    pde4
+    pde4
+    pde4
+    pde4
+    djnz clear_screen_loop
+    ld sp,hl                ; restore SP
+    ei
     ret
