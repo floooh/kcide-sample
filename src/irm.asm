ebc4d9fbea4c0b2b12f62db524039bdf657465cb
Andre Weissflog <floooh@gmail.com>
2023-12-14 22:45:42 +0100

    coloring wip

--- src/irm.asm
+++ src/irm.asm
@@ -8,6 +8,31 @@ IO84_HICOLOR        = (1<<3)
 IO84_SEL_RAM8       = (1<<4)
 IO84_BLOCKSEL_RAM8  = (1<<5)
 
+BG_BLACK        = 0
+BG_BLUE         = 1
+BG_RED          = 2
+BG_PINK         = 3
+BG_GREEN        = 4
+BG_TEAL         = 5
+BG_YELLOW       = 6
+BG_GREY         = 7
+
+FG_BLUE         = (1<<3)
+FG_RED          = (2<<3)
+FG_PINK         = (3<<3)
+FG_GREEN        = (4<<3)
+FG_TEAL         = (5<<3)
+FG_YELLOW       = (6<<3)
+FG_WHITE        = (7<<3)
+FG_BLACK        = (8<<3)
+FG_VIOLET       = (9<<3)
+FG_ORANGE       = (10<<3)
+FG_PURPLE       = (11<<3)
+FG_GREENBLUE    = (12<<3)
+FG_BLUEGREEN    = (13<<3)
+FG_YELLOWGREEN  = (14<<3)
+FG_WHITE2       = (15<<3)
+
 ; write to pixel bank 0
 ; trashes: a
 access_pixels_0:
--- src/main.asm
+++ src/main.asm
@@ -1,24 +1,103 @@
     org 200h
 
+SCROLL_ADDR = 80E0h
+
     ld a,60h
     call cls_1
     call display_1
+    call write_colors
     call vsync_init
     ld hl,scroll_text
     call scroll_init
 
 .frame_loop:
     call scroll_begin_frame
-    ld e,C0h
+    ld e,[L(SCROLL_ADDR)]
     call scroll_draw
     call scroll_end_frame
 
     call vsync_wait
     jr .frame_loop
 
+write_colors:
+    call access_colors_1
+    ld de,SCROLL_ADDR
+    ld b,28h
+.loop:
+    push bc
+    ld hl,colors
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    ldi
+    inc d
+    ld e,[L(SCROLL_ADDR)]
+    pop bc
+    djnz .loop
+    call access_pixels_1
+    ret
+
     include "irm.asm"
     include "vsync.asm"
     include "scroll.asm"
+
+colors:
+    db BG_BLACK | FG_YELLOW
+    db BG_BLACK | FG_YELLOW
+    db BG_BLACK | FG_YELLOW
+    db BG_BLACK | FG_YELLOWGREEN
+    db BG_BLACK | FG_YELLOW
+    db BG_BLUE  | FG_YELLOWGREEN
+    db BG_BLACK | FG_GREEN
+    db BG_BLUE  | FG_YELLOWGREEN
+    db BG_BLACK | FG_GREEN
+    db BG_BLUE  | FG_GREENBLUE
+    db BG_BLUE  | FG_TEAL
+    db BG_BLACK | FG_GREENBLUE
+    db BG_BLUE  | FG_TEAL
+    db BG_BLUE  | FG_BLUEGREEN
+    db BG_BLACK | FG_BLUEGREEN
+    db BG_BLUE  | FG_BLUEGREEN
+    db BG_BLUE  | FG_BLUEGREEN
+    db BG_BLUE  | FG_BLUEGREEN
+    db BG_BLACK | FG_BLUEGREEN
+    db BG_BLUE  | FG_BLUEGREEN
+    db BG_BLUE  | FG_BLUEGREEN
+    db BG_BLUE  | FG_BLUEGREEN
+    db BG_BLUE  | FG_BLUEGREEN
+    db BG_BLACK | FG_BLUEGREEN
+    db BG_BLUE  | FG_BLUEGREEN
+    db BG_BLUE  | FG_BLUEGREEN
+    db BG_BLUE  | FG_BLUEGREEN
+    db BG_BLUE  | FG_BLUEGREEN
+    db BG_BLUE  | FG_BLUEGREEN
+    db BG_BLUE  | FG_BLUEGREEN
 
 scroll_text:
     DB "*** The KC85 was a series of 8-bit computers built in East Germany during the 1980's. "
