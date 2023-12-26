46ebd5cae08858f2cca9c34ea2d0c12064a5d1c0
Andre Weissflog <floooh@gmail.com>
2023-12-26 15:37:48 +0100

    c64 example project done

--- c64/src/main.asm
+++ c64/src/main.asm
@@ -1,7 +1,9 @@
-POSX = 80
-POSY = 80
-WIDTH = 24
-HEIGHT = 21
+WIDTH = 24          ; sprite width
+HEIGHT = 21         ; sprite height
+ORIGIN_X = 24       ; top-left corner sprite x coordinate
+ORIGIN_Y = 50       ; top-left corner sprite y coordinate
+MAX_X = 254 - WIDTH
+MAX_Y = 200
 
 
     ; C64 PRGs usually start at address $801
@@ -14,17 +16,16 @@ HEIGHT = 21
 _start:                 ; execution starts here
     jsr init_colors
     jsr init_sprites
-.loop:
-    jmp .loop
+.frame:
+    jsr update_pos
+    jsr set_sprite_pos
+    jsr wait_raster
+    jmp .frame
 
 init_sprites:
-    lda #15
-    sta $d015           ; enable sprite 0..3
-;    sta $d01D           ; double width
-;    sta $d017           ; double height
-    sta $d01b           ; priority behind foreground
+    jsr set_sprite_pos
 
-    ldx #(sprite0/64)   ; sprite data pointer
+    ldx #(sprite0/64)   ; sprite data pointers
     stx $07f8
     inx
     stx $07f9
@@ -33,26 +34,18 @@ init_sprites:
     inx
     stx $07fb
 
-    lda #POSX           ; position
-    sta $d000           ; sprite0 posx
-    sta $d004           ; sprite2 posx
-    lda #POSX+WIDTH
-    sta $d002           ; sprite1 posx
-    sta $d006           ; sprite3 posx
-    lda #POSY
-    sta $d001           ; sprite0 posy
-    sta $d003           ; sprite1 posy
-    lda #POSY+HEIGHT
-    sta $d005           ; sprite2 posy
-    sta $d007           ; sprite3 posy
-
-    lda #14             ; bright-blue color
+    lda #14             ; bright-blue color for sprites 0..2
     sta $d027
     sta $d028
     sta $d029
-    lda #10             ; bright-red color
+    lda #10             ; bright-red color for sprite 3
     sta $d02A
 
+    lda #15
+    sta $d015           ; enable sprite 0..3
+;    sta $d01D           ; double width
+;    sta $d017           ; double height
+    sta $d01b           ; priority behind foreground
     rts
 
 init_colors:
@@ -70,6 +63,77 @@ init_colors:
     dex
     bne .loop
     rts
+
+set_sprite_pos:
+    clc
+    lda pos_x
+    sta $d000           ; sprite0 posx
+    sta $d004           ; sprite2 posx
+    adc #WIDTH
+    sta $d002           ; sprite1 posx
+    sta $d006           ; sprite3 posx
+    lda pos_y
+    sta $d001           ; sprite0 posy
+    sta $d003           ; sprite1 posy
+    adc #HEIGHT
+    sta $d005           ; sprite2 posy
+    sta $d007           ; sprite3 posy
+    rts
+
+update_pos:
+    lda pos_x
+    clc
+    adc dx
+    sta pos_x
+    cmp #MAX_X
+    beq .bounce_x
+    cmp #ORIGIN_X
+    beq .bounce_x
+.update_y:
+    lda dy_lo
+    clc
+    adc #$60
+    sta dy_lo
+    bcc .no_overflow
+    inc dy_hi
+.no_overflow:
+    lda pos_y
+    clc
+    adc dy_hi
+    sta pos_y
+    cmp #MAX_Y
+    bcs .bounce_y
+.done:
+    rts
+
+.bounce_x:
+    sec
+    lda #0
+    sbc dx
+    sta dx
+    jmp .update_y
+.bounce_y:
+    lda #0
+    sta dy_lo
+    sec
+    sbc dy_hi
+    sta dy_hi
+    lda #MAX_Y
+    sta pos_y
+    jmp .done
+
+wait_raster:
+.loop:
+    lda $d012
+    cmp #$ff
+    bne .loop
+    rts
+
+pos_x:  db ORIGIN_X
+pos_y:  db ORIGIN_Y
+dy_lo:  db 0
+dy_hi:  db 0
+dx:     db 2
 
     ; top-left quadrant
     align $40
